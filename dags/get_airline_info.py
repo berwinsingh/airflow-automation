9e65d521fe309349c08fffea725e025b332e17ed
Berwin Singh <63018717+berwinsingh@users.noreply.github.com>
2024-10-09 23:38:13 +0530

    added comments for better understanding

--- dags/get_airline_info.py
+++ dags/get_airline_info.py
@@ -1,9 +1,8 @@
 from airflow import DAG
+from functions.extract_data import get_airlines, save_to_supabase, log_dag_execution
 from airflow.operators.python import PythonOperator, BranchPythonOperator
-from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from datetime import datetime, timedelta
-from functions.extract_data import get_airlines, save_to_supabase, log_dag_execution
 
 default_args = {
     'owner': 'berwin_singh',
@@ -22,7 +21,7 @@ def log_dag_end(**context):
 def check_none(**context):
     ti = context['ti']
     data = ti.xcom_pull(task_ids='get_airlines_task')
-    #Using the task_id to determine the next step in the DAG
+    #Using the task_id to determine the next step in the DAG based on the data being pulled from the get_airlines_task
     if data is None:
         return 'end_dag'
     else:
@@ -33,26 +32,21 @@ with DAG(
     default_args=default_args,
     description='A simple DAG to extract airline information and save to Supabase',
     schedule_interval='@hourly',
-    catchup=False
+    catchup=False #Catchup is set to False to prevent the DAG from running on all the previous dates
 ) as dag:
     
     task_get_airlines = PythonOperator(
         task_id='get_airlines_task',
         python_callable=get_airlines,
         op_kwargs={'airline_number': 'LH401'},
-        do_xcom_push=True
+        do_xcom_push=True #Pushing the data to the next task
     )
     
     task_save_airlines = PythonOperator(
         task_id='save_airlines_task',
         python_callable=save_to_supabase,
         op_kwargs={'data': "{{ task_instance.xcom_pull(task_ids='get_airlines_task') }}"},
-        provide_context=True
-    )
-    
-    task_hello = BashOperator(
-        task_id='hello',
-        bash_command='echo "Hello, this is a bash task"'
+        provide_context=True #Providing the context to the next task
     )
 
     start_log = PythonOperator(
@@ -67,16 +61,16 @@ with DAG(
         provide_context=True
     )
 
-    check_data = BranchPythonOperator(
+    check_data = BranchPythonOperator( #Being used to determine the next step in the DAG kind of like if else statement
         task_id='check_data',
         python_callable=check_none,
         provide_context=True
     )
 
     end_dag = DummyOperator(
-        task_id='end_dag'
+        task_id='end_dag' #Ending the DAG
     )
 
     task_get_airlines >> start_log >> check_data
     check_data >> [task_save_airlines, end_dag]
-    task_save_airlines >> task_hello >> end_log
+    task_save_airlines >> end_log
